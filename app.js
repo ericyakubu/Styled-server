d76a7d423dd18134501738a0a6a3674a1f9df59c
CaxaROCK <yakubueric@gmail.com>
2023-09-04 16:35:15 -0400

    cors url change

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   const allowedOrigins = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
-    "https://styled-eight.vercel.app/",
+    "https://styled-eight.vercel.app",
   ];
 
   // Check if the request's origin is in the allowed origins array
